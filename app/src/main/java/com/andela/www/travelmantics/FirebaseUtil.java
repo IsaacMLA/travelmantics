4c82069f7f175b085b67e56e2c18c2bcdfda5f73
Isaac Moché <isaaclouisarnaud@gmail.com>
2019-08-13 11:34:56 +0000

    sign in

--- app/src/main/java/com/andela/www/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/andela/www/travelmantics/FirebaseUtil.java
@@ -1,18 +1,30 @@
 package com.andela.www.travelmantics;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by DELL on 05/08/2019.
  */
 
 public class FirebaseUtil {
+    private static final int RC_SIGN_IN = 123 ;
     public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseRef;
     private static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
+    private static Activity caller;
     public static ArrayList<TravelDeal> mdeals;
 
 
@@ -22,8 +34,53 @@ public class FirebaseUtil {
         if(firebaseUtil == null){
             firebaseUtil = new FirebaseUtil();
             mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mFirebaseAuth = FirebaseAuth.getInstance();
         }
         mdeals = new ArrayList<TravelDeal>();
         mDatabaseRef = mFirebaseDatabase.getReference().child(ref);
+    }
+
+
+    public static void openFbReference(String ref, final Activity callerActivity){
+        if(firebaseUtil == null){
+            firebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mFirebaseAuth = FirebaseAuth.getInstance();
+            caller = callerActivity;
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        FirebaseUtil.signIn();
+                        Toast.makeText(callerActivity.getBaseContext(), "welcome bro", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            };
+        }
+        mdeals = new ArrayList<TravelDeal>();
+        mDatabaseRef = mFirebaseDatabase.getReference().child(ref);
+    }
+
+
+    private static void signIn(){
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
+// Create and launch sign-in intent
+        caller.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    public static void attachListener(){
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public static void detachListener(){
+        mFirebaseAuth.removeAuthStateListener(mAuthListener);
     }
 }
--- app/src/main/java/com/andela/www/travelmantics/ListActivity.java
+++ app/src/main/java/com/andela/www/travelmantics/ListActivity.java
@@ -28,13 +28,6 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-
-        RecyclerView rvDeals =  (RecyclerView) findViewById(R.id.rvDeals);
-        final DealAdapter adapter = new DealAdapter();
-        rvDeals.setAdapter(adapter);
-        LinearLayoutManager dealsLayoutManager =
-                new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        rvDeals.setLayoutManager(dealsLayoutManager);
     }
 
     @Override
@@ -53,5 +46,24 @@ public class ListActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseUtil.detachListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUtil.openFbReference("traveldeals",this);
+        RecyclerView rvDeals =  (RecyclerView) findViewById(R.id.rvDeals);
+        final DealAdapter adapter = new DealAdapter();
+        rvDeals.setAdapter(adapter);
+        LinearLayoutManager dealsLayoutManager =
+                new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        rvDeals.setLayoutManager(dealsLayoutManager);
+        FirebaseUtil.attachListener();
     }
 }
